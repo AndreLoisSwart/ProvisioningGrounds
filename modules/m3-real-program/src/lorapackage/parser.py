ab8e5de6f9762488ce821fe81474580a0672c597
AndreLoisSwart <149463901+AndreLoisSwart@users.noreply.github.com>
2026-09-15 13:18:04 +0200

    save progress

--- modules/m3-real-program/src/lorapackage/parser.py
+++ modules/m3-real-program/src/lorapackage/parser.py
@@ -1,4 +1,12 @@
+import logging
 from pathlib import Path
+
+
+class MalformedLogLineError(ValueError):
+    pass 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def build_path() -> Path:
@@ -8,15 +16,18 @@ def build_path() -> Path:
 def read_sensor_data(path: str | Path) -> list[dict]:
     sensor_data = []
     path = Path(path)
-
-    with open(path) as f:
-        for line in f:
-            new_line = line.split(",")
-            record = {
-                    "timestamp": new_line[0],
-                    "node": new_line[1],
-                    "temp":float(new_line[2].split("=")[1]),
-                    "batt": float(new_line[3].split("=")[1])
+    try:
+        with open(path) as f:
+            for line in f:
+                new_line = line.split(",")
+                record = {
+                        "timestamp": new_line[0],
+                        "node": new_line[1],
+                        "temp":float(new_line[2].split("=")[1]),
+                        "batt": float(new_line[3].split("=")[1])
                 }
-            sensor_data.append(record)
+                sensor_data.append(record)
+    except ValueError as e:
+        raise MalformedLogLineError(f"A value within the log line was malformed: {e}")
+    
     return sensor_data
