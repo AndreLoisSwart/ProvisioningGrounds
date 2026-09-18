88566f9f88563b9cfe2618ae2ea4491b1bf4de56
AndreLoisSwart <149463901+AndreLoisSwart@users.noreply.github.com>
2026-09-18 10:36:53 +0200

    argparse usage

--- modules/m3-real-program/src/lorapackage/__main__.py
+++ modules/m3-real-program/src/lorapackage/__main__.py
@@ -1,14 +1,29 @@
 import logging
+import argparse
 
 from .parser import build_path, read_sensor_data
 from .reporter import sensor_summary
 
-logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
 def main():
-    logger.info("main started")
-    data = read_sensor_data(build_path())
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", default=None,type=str, help="Provide a path to your file, or leave blank to use fallback.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Turn on debug logging")
+
+    args = parser.parse_args()
+
+    log_level= logging.DEBUG if args.verbose else logging.INFO
+
+    logging.basicConfig(level=log_level)
+
+    logger.info("This message always shows up.")
+    logger.debug("This message ONLY shows up if you used -v or --verbose.")
+
+    file_path = args.file or build_path()
+
+    data = read_sensor_data(file_path)
     sensor_summary(data)
 
 
